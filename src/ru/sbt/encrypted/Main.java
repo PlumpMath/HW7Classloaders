54479697feaf2a80a22eb1ccb12b51af583acb9f
Ярослав Котылев <yavkotylev@gmail.com>
2016-10-07 14:11:57 +0300

    some changes

--- src/ru/sbt/encrypted/Main.java
+++ src/ru/sbt/encrypted/Main.java
@@ -19,7 +19,6 @@ public class Main {
             String className = getClassName(encryptedClassLoader);
 
             switch (userCommand) {
-
                 case Read:
                     try {
                         readClass(encryptedClassLoader, className);
@@ -41,7 +40,7 @@ public class Main {
             try {
                 clazz.getMethod("doUsefull").invoke(clazz.newInstance());
             } catch (Exception e) {
-
+                throw new RuntimeException(e);
             }
         } catch (ClassNotFoundException e) {
             System.out.println("Can't find class");
@@ -51,16 +50,13 @@ public class Main {
 
     private static Command getCommand() {
         String commandStr;
-
         Scanner scanner = new Scanner(System.in);
-
         boolean rightCommand = false;
         Command userCommand = null;
         do {
             System.out.println("Choose command:\n1) \"Read\" - to read encrypted class\n2) " +
                     "\"Write\" - to encrypt class\n3) \"Exit\"");
             commandStr = scanner.next();
-
             for (Command command : Command.values()) {
                 if (command.name().equals(commandStr)) {
                     rightCommand = true;
@@ -69,8 +65,6 @@ public class Main {
                 }
             }
         } while (rightCommand == false);
-
-
         return userCommand;
     }
 
@@ -81,18 +75,15 @@ public class Main {
         do {
             System.out.println("\nWrite file path:");
             filePath = scanner.next();
-
             System.out.println("Write encrypting key:");
             key = scanner.next();
             file = new File(filePath);
-
             if (file.isDirectory() == false || file.exists() == false) {
                 System.out.println("Wrong filepath!");
             }
         } while ((file.exists() && file.isDirectory()) == false);
         EncryptedClassLoader encryptedClassLoader = new EncryptedClassLoader(key, file,
                 ClassLoader.getSystemClassLoader());
-
         return encryptedClassLoader;
     }
 
@@ -102,16 +93,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         File file;
         do {
-
             System.out.println("\nWrite class name:");
             className = scanner.next();
-
             if (encryptedClassLoader.getDir().getPath() != "") {
                 file = new File(encryptedClassLoader.getDir().getPath() + "/" + className + ".class");
             } else {
                 file = new File(encryptedClassLoader.getDir().getPath() + className + ".class");
             }
-
             if (file.isFile() == false) {
                 System.out.println("Wrong file name");
             }
@@ -119,9 +107,7 @@ public class Main {
                 System.out.println("File doesn't exist");
             }
         } while ((file.exists() && file.isFile()) == false);
-
         return className;
-
     }
 
     private enum Command {
